63117345a90e34793355e663e4ac168c81273cd8
Jason Wodicka <jason@wodicka.net>
2020-01-13 16:20:55 -0800

    default to Diavlo on Foolscap sign template

--- src/templates/FoolscapScheduleItem.js
+++ src/templates/FoolscapScheduleItem.js
@@ -4,7 +4,7 @@ import {WrapText} from '../svg';
 const ScheduleSign = React.forwardRef((props, ref) => (
   <svg ref={ref} version="1.1" xmlns="http://www.w3.org/2000/svg" width="11in" height="8.5in" viewBox="0 0 1100 850">
     <rect x="0" y="0" width="1100" height="850" stroke="black" fill="white" />
-    <WrapText text={props.panelName} x={50} y={200} width={1000} height={450} />
+    <WrapText text={props.panelName} x={50} y={200} width={1000} height={450} fontFamily="Diavlo" fontSize={150}/>
   </svg>
 ))
 ScheduleSign.varNames = ["panelName"]
